756ab01a0848ea2c0564be09da6ff8a6fe2aa296
ygh97 <yysu33@naver.com>
2024-06-04 17:47:52 +0900

    0604

---
+++ 01_WEB/03_vue/chapter11/todolist-app-router/src/pages/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    123123123
+  </div>
+</template>
---
+++ 01_WEB/03_vue/chapter11/todolist-app-router/src/pages/aadd.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="card card-body">
+    <h2>회원가입</h2>
+    123123123
+  </div>
+</template>
